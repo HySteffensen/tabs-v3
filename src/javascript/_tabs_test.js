1ac88f22b1625c8a8de46ca1d47ddb5aeb32ea73
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-13 17:31:43 -0700

    adds 'selected' to previously existing class

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -14,7 +14,12 @@
 		});
 
 		it("adds 'selected' to a previously existing class", function() {
-			
+			var element = document.createElement("div");
+			element.classList.add("previouslyExistingClass");
+			document.body.appendChild(element);
+			tabs.initialize(element);
+			assert.equal(element.getAttribute("class"), "previouslyExistingClass selected");
+			element.parentNode.removeChild(element);
 		});	
 		
 	});
