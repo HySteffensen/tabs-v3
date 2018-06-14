7c80ce50ea28c5a9490e0f4e40f8bedbfd4d60ad
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-13 17:46:16 -0700

    factored out addElement()

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -6,21 +6,25 @@
 
 	describe("Tabs", function() {
 		it("adds 'selected' class to an element", function() {
-			var element = document.createElement("div");
-			document.body.appendChild(element);
+			var element = addElement("div");
 			tabs.initialize(element);
 			assert.equal(element.getAttribute("class"), "selected");
 			element.parentNode.removeChild(element);
 		});
 
 		it("adds 'selected' to a previously existing class", function() {
-			var element = document.createElement("div");
-			element.classList.add("previouslyExistingClass");
-			document.body.appendChild(element);
+			var element = addElement("div");
+			element.setAttribute("class", "previouslyExistingClass");
 			tabs.initialize(element);
 			assert.equal(element.getAttribute("class"), "previouslyExistingClass selected");
 			element.parentNode.removeChild(element);
 		});	
+
+		function addElement(tagName) {
+			var element = document.createElement(tagName);
+			document.body.appendChild(element);
+			return element;
+		}
 		
 	});
 }());
